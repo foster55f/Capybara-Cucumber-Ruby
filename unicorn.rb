a5eed87fddf01e2c5698704ec0f0581f55c6557b
“Foster <fosterataylor@yahoo.com>
2020-06-16 11:22:25 -0600

    Add spacing to Unicorn class

--- unicorn.rb
+++ unicorn.rb
@@ -4,12 +4,15 @@ class Unicorn
       @name = name_parameter
       @color = color_parameter
     end
+
     def white?
       @color == "white"
     end
+
     def say(phrase)
       "**;* #{phrase} **;*"
     end
+    
     def say_name
       "My name is #{@name}"
     end
